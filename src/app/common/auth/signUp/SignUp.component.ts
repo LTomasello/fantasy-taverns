9f4627de44031d31523906ec32172a3e2d312dfa
Louis Tomasello <LTomasello@milestraining.lab>
2019-10-16 18:48:18 -0400

    Inserted Radio Buttons

--- src/app/common/auth/signUp/SignUp.component.ts
+++ src/app/common/auth/signUp/SignUp.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { RadioControlValueAccessor } from '@angular/forms';
 
 @Component({
     templateUrl: './SignUp.component.html',
@@ -8,17 +9,25 @@ import { AuthService } from '../auth.service';
 export class SignUpComponent {
     userName = '';
     password = '';
+    Manager = '';
+    Admin = '';
 
     constructor(private router: Router, private authService: AuthService) {}
 
     signUp(): void {
+        const user = {userName: 'ltomasello2462@gmail.com', password: 'cloud7'};
+            this.authService.create(user).subscribe((answer) => {
+                this.router.navigateByUrl('/login')
         
-        console.log(this.userName);
-        console.log(this.password);
+        });
     }
 
     cancel(): void{
         this.router.navigateByUrl('/login');
+    }
+
+    admin(): void{
+
     }
 
 }
