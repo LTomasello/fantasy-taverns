c8f079fefe25243825004560c34b075d74901e34
Louis Tomasello <LTomasello@milestraining.lab>
2019-10-18 14:24:09 -0400

    Finished Homework 2

--- src/app/common/auth/signUp/SignUp.component.ts
+++ src/app/common/auth/signUp/SignUp.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
-import { RadioControlValueAccessor } from '@angular/forms';
+import { TavernService } from '../../tavern.service';
+import { Identifiers } from '@angular/compiler/src/render3/r3_identifiers';
 
 @Component({
     templateUrl: './SignUp.component.html',
@@ -9,25 +10,65 @@ import { RadioControlValueAccessor } from '@angular/forms';
 export class SignUpComponent {
     userName = '';
     password = '';
-    Manager = '';
-    Admin = '';
+    role ='';
+    tavernName = ''
+    taverns = [];
+    selectTavern = null;
+   
 
-    constructor(private router: Router, private authService: AuthService) {}
+    constructor(private router: Router, private authService: AuthService, private tavernService: TavernService) {
+
+        this.tavernService.getTaverns().subscribe(
+            (Response) => {
+                console.log(Response);
+                this.taverns = Response;
+            },
+            (error) => {
+                console.log(error);
+            }
+            
+        )
+    }
 
     signUp(): void {
-        const user = {userName: 'ltomasello2462@gmail.com', password: 'cloud7'};
+        // work from class
+        /*const user = {userName: 'ltomasello2462@gmail.com', password: 'cloud7'};
             this.authService.create(user).subscribe((answer) => {
                 this.router.navigateByUrl('/login')
         
-        });
-    }
+        });*/
 
+        let user: {};
+
+        if (this.role === 'admin'){
+            user = {
+                UserName: this.userName,
+                Password: this.password,
+                Tavern: {
+                    ID: 0,
+                    tavernName: this.tavernName
+                }
+            }
+        }
+        else if (this.role = 'manager') {
+            user = {
+                UserName: this.userName,
+                Password: this.password,
+                Tavern: this.selectTavern
+            }
+        }
+        console.log(this.userName, this.password, this.tavernName);
+
+    }
+    
+    
     cancel(): void{
         this.router.navigateByUrl('/login');
     }
 
-    admin(): void{
-
+    roleValue() {
+        console.log('ngModel value', this.role );
     }
 
 }
+
